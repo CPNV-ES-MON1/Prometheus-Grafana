2b75ae2d5f8c367073f96fb818727db48d8eab87
cpnv <amos.lecoq@eduvaud.ch>
2025-06-09 14:09:30 +0200

    Correction_Script_AddClient

--- Debian/Script/ajout_client.sh
+++ Debian/Script/ajout_client.sh
@@ -1,15 +1,15 @@
-
 #!/bin/bash
 
-# Demande des valeurs à l'utilisateur
+# Demande des valeurs ?? l'utilisateur
 read -p "Entrez le nom du job (default_name) : " job_name
 read -p "Entrez l'adresse IP (default_ip) : " ip_address
 
 # Chemin du fichier
-file="Config/prometheus.yml"
+file="../Config/prometheus.yml"
 
-# Ajout des lignes à la fin du fichier avec indentation adaptée
+# Ajout des lignes ?? la fin du fichier avec indentation adapt??e
 cat <<EOF >> "$file"
+
   - job_name: '$job_name'
     honor_timestamps: true
     scrape_interval: 15s
@@ -21,3 +21,5 @@ cat <<EOF >> "$file"
 EOF
 
 echo "Ligne add of end of file: $file."
+
+
